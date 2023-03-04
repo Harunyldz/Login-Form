ea329100fcb6483073753369cde52025807a2440
“harun <h.yildiz1217@gmail.com>
2023-03-04 21:22:01 +0300

    SignUp added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
 function Login(){
     document.getElementById('join').style.display='none';
     document.getElementById('login').style.display='flex';  
     document.getElementById('body').style.background='url(/images/background5.jpg)';
-    document.getElementById('body').style.backgroundSize='cover';
-    
+    document.getElementById('body').style.backgroundSize='cover';    
+}
+function Enter(){
+let username=document.getElementById('input-user').value;
+let password=document.getElementById('input-pass').value;
+alert('Username: '+username +'\n' + 'Password : '+password);
 }
