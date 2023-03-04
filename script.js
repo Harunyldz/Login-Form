a13c233dae538df74f8a6da5a9c829bc5e98df98
“harun <h.yildiz1217@gmail.com>
2023-03-04 18:34:48 +0300

    first initialize

---
+++ script.js
@@ -0,0 +1,7 @@
+function Login(){
+    document.getElementById('join').style.display='none';
+    document.getElementById('login').style.display='flex';  
+    document.getElementById('body').style.background='url(/images/background5.jpg)';
+    document.getElementById('body').style.backgroundSize='cover';
+    
+}
